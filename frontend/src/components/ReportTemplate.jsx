fdb8ec6d6db1637254ac5a25a4107e11d4f7e25a
Developmentexpert121 <developmentexpert121@gmail.com>
2025-06-29 16:17:21 +0530

    issues fixed

--- frontend/src/components/ReportTemplate.jsx
+++ frontend/src/components/ReportTemplate.jsx
@@ -61,7 +61,7 @@ function ReportTemplate() {
     try {
       const token = Cookies.get("token");
       const response = await fetch(
-        "https://medical-report-editor-ai-powered-backend.onrender.com/compile-report",
+        "https://medical-report-editor-ai-powered-backend.onrender.com/compile-report-stream",
         {
           method: "POST",
           body: formDataToSend,
